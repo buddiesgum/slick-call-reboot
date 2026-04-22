791e268ba3773143ef68bfdd882939758a1885ca
AJ Caldwell <tomatrow@gmail.com>
2026-04-21 21:34:51 -0700

    Disable some linting erros for shadcn ui

--- eslint.config.js
+++ eslint.config.js
@@ -22,5 +22,12 @@ export default tseslint.config(
 			"react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
 			"@typescript-eslint/no-unused-vars": "off"
 		}
+	},
+	{
+		files: ["src/components/ui/**/*.{ts,tsx}"],
+		rules: {
+			"react-refresh/only-export-components": "off",
+			"@typescript-eslint/no-empty-object-type": "off"
+		}
 	}
 )
