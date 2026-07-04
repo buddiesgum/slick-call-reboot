2a613b7bb7ff07ef17c2719f257836fc37ce2b2a
AJ Caldwell <tomatrow@gmail.com>
2026-07-03 21:50:29 -0700

    Remove www-old

--- packages/www-old/worker/lib/html.ts
+++
@@ -1,9 +0,0 @@
-/** Minimal HTML escaping for user-supplied values interpolated into email bodies. */
-export function escapeHtml(str: string): string {
-	return str
-		.replace(/&/g, "&amp;")
-		.replace(/</g, "&lt;")
-		.replace(/>/g, "&gt;")
-		.replace(/"/g, "&quot;")
-		.replace(/'/g, "&#39;")
-}
